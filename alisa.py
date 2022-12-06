7e98dd6e999bc8d4ac57dfc0529434f0a6a9fbae
Alisa Rubinstein <alisarubinstein@Alisas-MacBook-Air.local>
2022-12-06 11:48:07 -0500

    up until the sql database works

--- alisa.py
+++ alisa.py
@@ -1,7 +1,3 @@
-import json
-import unittest
-import os
-import requests
 
 import unittest
 import sqlite3
@@ -9,14 +5,9 @@ import json
 import os
 import matplotlib.pyplot as plt
 
-API_KEY = "4OYHGD36AJLqzXGiE3TyEVLeplNvAT7W"
-JSON_LINK = "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=4OYHGD36AJLqzXGiE3TyEVLeplNvAT7W"
+API_KEY = "MzA4MzEwNTV8MTY3MDM0MDc3My44MTMzMjk"
+JSON_LINK = "https://api.seatgeek.com/2/events?client_id=MzA4MzEwNTV8MTY3MDM0MDc3My44MTMzMjk&per_page=100"
 
-# def get_flight_info():
-#     r = requests.get("https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=4OYHGD36AJLqzXGiE3TyEVLeplNvAT7W")
-#     print(r)
-#     j= json.loads(r.text)
-#     return j
 
 
 def setUpDatabase(db_name):
@@ -25,24 +16,35 @@ def setUpDatabase(db_name):
     cur = conn.cursor()
     return cur, conn
 
-def add_events_from_json(filename):
+def create_events_table(cur, conn):
+    cur.execute('CREATE TABLE IF NOT EXISTS seatgeek (id INTEGER PRIMARY KEY, type TEXT, name TEXT, time TEXT, state TEXT)')
+    conn.commit()
+
+def add_events_from_json(filename, cur, conn):
     f = open(filename)
     file_data = f.read()
     f.close()
     json_data = json.loads(file_data)
-    return json_data
-    
+    # print(len(json_data["events"]))
+
+    # pick info to put in db, list and then add to dictionary 
+    for items in json_data["events"]:
+        id = items["id"]  #primary key bc its unique - for the insert or ignore 
+        type = items['type']
+        name = items['venue']['name']
+        time = items["datetime_utc"]
+        state = items['venue']['state']
+        cur.execute('INSERT OR IGNORE INTO seatgeek (id, type, name, time, state) VALUES (?,?,?,?,?)', (id, type, name, time, state))
+        conn.commit()
+
+
 
 def main():
     # get_flight_info()
-    setUpDatabase('events_db')
-    add_events_from_json('events.json')
+    cur, conn = setUpDatabase('EVENTS_db')
+    create_events_table(cur, conn)
+    add_events_from_json('events.json', cur, conn)
     pass
 
 if __name__ == "__main__":
     main()
-
-# def main():
-#     find_flight_info(API_KEY)
-
-# print(main)
