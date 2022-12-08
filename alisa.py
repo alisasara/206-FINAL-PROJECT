833ef19892a0b68a7c0c0d1c648346544f2a56e9
Alisa Rubinstein <alisarubinstein@Alisas-MacBook-Air.local>
2022-12-08 12:38:24 -0500

    donewith alisa.py

--- alisa.py
+++ alisa.py
@@ -3,9 +3,11 @@ import unittest
 import sqlite3
 import json
 import os
+import requests
 import matplotlib.pyplot as plt
 
 API_KEY = "MzA4MzEwNTV8MTY3MDM0MDc3My44MTMzMjk"
+page_num = "100"
 JSON_LINK = "https://api.seatgeek.com/2/events?client_id=MzA4MzEwNTV8MTY3MDM0MDc3My44MTMzMjk&per_page=100"
 
 
@@ -35,36 +37,50 @@ def create_events_table(cur, conn):
     cur.execute('CREATE TABLE IF NOT EXISTS seatgeek (id INTEGER PRIMARY KEY, type TEXT, name TEXT, venue TEXT, time TEXT, state TEXT)')
     conn.commit()
 
-def add_events_from_json(filename, cur, conn):
-    f = open(filename)
-    file_data = f.read()
-    f.close()
-    json_data = json.loads(file_data)
+def add_events_from_json(cur, conn):
+    base_url  = "https://api.seatgeek.com/2/events?client_id={}&per_page={}"
+    request_url = base_url.format(API_KEY, page_num )
+    param_dict = {'format': 'json'}
+    r = requests.get(request_url, params = param_dict)
+    data = r.text
+    json_data = json.loads(data) # decoding JSON file
+    
 
+    ids = []
     types = []
     states = []
     venues = []
+    times = []
+    names = []
+    numbers = 1
     for items in json_data["events"]:
-        id = items["id"]  #primary key bc its unique - for the insert or ignore 
+        id = numbers
+        numbers += 1 
+        ids.append(id)
         type = items['type']
         types.append(type)
         venue = items['venue']['name']
         venues.append(venue)
         time = items["datetime_utc"]
+        times.append(time)
         state = items['venue']['state']
         states.append(state)
         name = items['performers'][0]['name']
-        
+        names.append(name)
+        # add in counter variable  and then have a for loop with if statement to break if counter = 25 
+        # 25 count limit 
+    counter = cur.execute("SELECT max(id) FROM seatgeek").fetchone()[0]
+    print(counter)
+    if counter == None: 
+        counter = 0
+    for i in range(counter, counter + 25):
         # seatgeek main table info
-        cur.execute('INSERT OR IGNORE INTO seatgeek (id, type, name, venue, time, state) VALUES (?,?,?,?,?,?)', (id, type, name, venue, time, state))
-        conn.commit()
+        try: 
+            cur.execute('INSERT OR IGNORE INTO seatgeek (id, type, name, venue, time, state) VALUES (?,?,?,?,?,?)', (ids[i], types[i], names[i], venues[i], times[i], states[i]))
+        except: 
+            print("exceeds 100")
     
-    # 25 count limit 
-    cur.execute("SELECT * FROM seatgeek")
-    counter = cur.fetchall()
-    print(len(counter))
-
-    # info for types 
+    
     types_dict = {}
     for t in types:
         if t in types_dict:
@@ -107,16 +123,27 @@ def add_events_from_json(filename, cur, conn):
         conn.commit()
 
 
+def count_item_in_data_base(cur, conn):
+    cur.execute("SELECT * FROM seatgeek")
+    items = cur.fetchall()
+    return len(items)
+        
+      
+   
+
+   
+
 # query db- sele t count of column or sleect everything and count in python and then returns number - thne input 25 
 # index events from [x:x]
 
 def main():
-    cur, conn = setUpDatabase('EVENTS_db')
+    cur, conn = setUpDatabase('TRAVEL_db')
     create_events_table(cur, conn)
     create_types_table(cur, conn)
     create_states_table(cur, conn)
     create_venues_table(cur, conn)
-    add_events_from_json('events.json', cur, conn)
+    count_item_in_data_base(cur, conn)
+    add_events_from_json(cur, conn)
 
 
 if __name__ == "__main__":
